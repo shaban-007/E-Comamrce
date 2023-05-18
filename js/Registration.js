16244c8f8f759c2fef05eea51163b88b00aaf228
ahmed shaban <ahmed221821@gmail.com>
2023-05-18 23:26:33 +0200

    new add

--- js/Registration.js
+++ js/Registration.js
@@ -58,4 +58,4 @@ document.addEventListener('DOMContentLoaded', function() {
               localStorage.setItem("registrationData", registrationDataJSON);
 
             }
-             
+            
